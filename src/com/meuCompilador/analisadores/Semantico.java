19825e6c3fdd1fed1c2bb72278d1f6659c3c8a73
Leonardo Queiroz <leonardoqueiroz322@gmail.com>
2024-08-20 13:42:43 -0300

    semantico verifica se variaveis foram criadas, iniciadas e verifica divisão por zero

--- src/com/meuCompilador/analisadores/Semantico.java
+++ src/com/meuCompilador/analisadores/Semantico.java
@@ -11,33 +11,58 @@ public class Semantico {
 
     private static List<Token> vars;
     public static boolean check(List<Token> list) throws SemanticoException {
+        List<Token> initializedVar = new ArrayList<>();
         vars = new ArrayList<>();
         for (int i = 0; i < list.size();i++ ){
             if (list.get(i).getType() == TypeToken.LABEL_VAR){
                 while (list.get(i).getType() != TypeToken.SEMICOLON) {
                     if (list.get(i).getType() == TypeToken.ID){
-                        vars.add(list.get(i));
+                        if (vars.contains(list.get(i))){
+                            throw new SemanticoException("variavel [" + list.get(i).getLexema() +"] na posição " + list.get(i).getPosition()+" já foi criada.", vars);
+                        }else{
+                            vars.add(list.get(i));
+                        }
                     }
                     i++;
                 }
             }
-            
-            if (list.get(i).getType() == TypeToken.ID && !vars.contains(list.get(i))){
-                throw new SemanticoException("variavel [" + list.get(i).getLexema() +"] na posição "+ list.get(i).getPosition() +" não foi criada", vars);
+             
+            if (list.get(i).getType() == TypeToken.ID){
+                if (!vars.contains(list.get(i))){
+                    throw new SemanticoException("variavel [" + list.get(i).getLexema() +"] na posição "+ list.get(i).getPosition() +" não foi criada", vars);
+                    
+                }
+                if (list.get(i-2).getType() == TypeToken.READ){
+                    initializedVar.add(list.get(i));
+                }
+                if (list.get(i+1).getType() != TypeToken.ASSIGN){
+                    if (!initializedVar.contains(list.get(i))){
+                        throw new SemanticoException("variavel [" + list.get(i).getLexema() + "] na posição " + list.get(i).getPosition() + " não foi inicializada.", vars);
+                    }
+                } 
             }
-
+        
             if (list.get(i).getType() == TypeToken.ASSIGN){
-                int j = 0;
-                while (list.get(j).getType() != TypeToken.SEMICOLON) {
-                    if (list.get(j).getType() == TypeToken.ARITHMETIC && list.get(j).getLexema().equals("/")){
-                        if (list.get(j+1).getType() == TypeToken.NUMBER ){
+               int x = i;
+                while (list.get(i).getType() != TypeToken.SEMICOLON) {
+                    if (list.get(i).getType() == TypeToken.ID){
+                        if (!vars.contains(list.get(i))){
+                            throw new SemanticoException("variavel [" + list.get(i).getLexema() +"] na posição "+ list.get(i).getPosition() +" não foi criada", vars);
+                        }
+                        if (!initializedVar.contains(list.get(i))){
+                            throw new SemanticoException("variavel [" + list.get(i).getLexema() + "] na posição " + list.get(i).getPosition() + " não foi inicializada.", vars);
+                        }
+                    }
+                    if (list.get(i).getType() == TypeToken.ARITHMETIC && list.get(i).getLexema().equals("/")){
+                        if (list.get(i+1).getType() == TypeToken.NUMBER ){
                             if(Double.parseDouble(list.get(i+1).getLexema()) == 0){
-                                throw new SemanticoException("divisão por zero", vars);
+                                throw new SemanticoException("divisão por zero em " + list.get(i+1).getPosition(), vars);
                             }
                         }
                     }
-                    j++;
+                    i++;
                 }
+                initializedVar.add(list.get(x-1));
             }
         }
         return true;
